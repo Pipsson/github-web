cd16b47f55ca35f488c05978a3fa1ba46c3611d3
Pipsson <juliusmallya8420@gmail.com>
2025-06-04 14:35:00 +0300

    Starting project and addition of navbar component

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import  Navbar from  './components/Navbar.jsx'
+
+function App() {
+
+  return (
+    <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main>
+            Content
+          </main>
+        </div>
+    </Router>
+  )
+}
+
+export default App
