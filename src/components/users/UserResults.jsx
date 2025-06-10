51fe5baa98dada85b50f48fe1febe18c0da562e2
Pipsson <juliusmallya8420@gmail.com>
2025-06-10 11:18:35 +0300

    refactor:additional of the UserItem for displaying user Info

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import {useEffect} from 'react';
 import Spinner from '../layout/Spinner.jsx';
+import UserItem from '../users/UserItem.jsx'
 
 function UserResults() {
-    // since we are using the vite for installing react, then the env variable start with VITE
+    // since we are using the vite for installing reacting, then the env variable start with VITE
     const apiUrl =import.meta.env.VITE_API_URL;
     const apiToken = import.meta.env.VITE_GITHUB_TOKEN;
     console.log(apiUrl);
@@ -38,10 +39,13 @@ function UserResults() {
          return (
              <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
                  {
-                     users.map((user)=>(
-                         <h3>
-                             {user.login}
-                         </h3>
+                     users.map((user)=>(                          
+                         <UserItem key={ 
+                             user.id
+                         }
+                                   user ={user}
+                                   
+                         />
                      ))
                  }
              </div>
