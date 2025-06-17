40fd0e78595a8d3dc99763e077890c9d8ba2893e
Pipsson <juliusmallya8420@gmail.com>
2025-06-17 15:00:25 +0300

    feat: enhance User component with detailed user information and loading state management

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,145 +1,122 @@
-import {createContext, useEffect, useReducer, useState} from "react";
-import React from 'react';
+import { createContext, useEffect, useReducer, useState } from "react";
+import React from "react";
 import githubReducer from "./GithubReducer.js";
-import {useSearchParams} from "react-router-dom";
 
+const GithubContext = createContext();
 
-const  GithubContext = createContext();
+const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_GITHUB_TOKEN;
 
-const  apiUrl = import.meta.env.VITE_API_URL;
-const  apiKey = import.meta.env.VITE_GITHUB_TOKEN;
+export const GithubProvider = ({ children }) => {
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
+  const initialState = {
+    users: [],
+    user: {},
+    loading: false,
+    ripos : [],
+  };
 
-export const    GithubProvider = ({children}) =>{
-        // const [users, setUsers] = useState([]);
-        // const [loading, setLoading] = useState(true);
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
-    const initialState = {
-        users : [],
-        user : {},
-        loading : false,
-    }
-    
-    
-    const [state,dispatch] = useReducer(githubReducer, initialState);
+  // this was a function to fetch all users
+  // const fetchUsers =async ()=>{
+  //     const response = await fetch(
+  //         `${apiUrl}/users`,{
+  //             headers : {
+  //                 authorization : `token ${
+  //                     apiKey
+  //                 }`
+  //             }
+  //         }
+  //
+  //     )
+  //     const  data = await response.json();
+  //
+  //
+  //     //dispatch function to update state
+  //     dispatch ({
+  //         type : 'GET_USERS',
+  //         payload : data,
+  //     })
+  //     // setUsers(data);
+  //     // setLoading(false);
+  // }
 
-    
-    // this was a function to fetch all users
-    // const fetchUsers =async ()=>{
-    //     const response = await fetch(
-    //         `${apiUrl}/users`,{
-    //             headers : {
-    //                 authorization : `token ${
-    //                     apiKey
-    //                 }`
-    //             }
-    //         }
-    //
-    //     )
-    //     const  data = await response.json();
-    //
-    //    
-    //     //dispatch function to update state
-    //     dispatch ({
-    //         type : 'GET_USERS',
-    //         payload : data,
-    //     })
-    //     // setUsers(data);
-    //     // setLoading(false); 
-    // }
+  //searchUsers function using query parameter
+  const getUser = async (login) => {
+    setLoading();
 
-    //searchUsers function using query parameter
-    const searchUsers = async (login)=>{ 
-        setLoading();
-       
-        const response = await fetch(
-            `${apiUrl}/search/user/${login}}`,{
-                headers : {
-                    authorization : `token ${
-                        apiKey
-                    }`
-                }
-            }
-
-        )
-        const  data = await response.json();
-        if (response.status === 404 ) {
-            window.location = '/notfound'
-        }
-        else {
-             //dispatch function to update state
-        dispatch ({
-            type : 'GET_USER',
-            payload : items,
-        })
-        }
-
-
-        //dispatch function to update state
-        dispatch ({
-            type : 'GET_USER',
-            payload : items,
-        })
-        // setUsers(data);
-        // setLoading(false);
+    const response = await fetch(`${apiUrl}/users/${login}}`, {
+      headers: {
+        authorization: `token ${apiKey}`,
+      },
+    });
+    const data = await response.json();
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      //dispatch function to update state
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
     }
 
-    // function to get user
-     const getUser = async (text)=>{ 
-        setLoading();
-        const params = new URLSearchParams({
-            q:text
-        });
-        const response = await fetch(
-            `${apiUrl}/search/users?${params}`,{
-                headers : {
-                    authorization : `token ${
-                        apiKey
-                    }`
-                }
-            }
+    //dispatch function to update state
 
-        )
-        const  {items} = await response.json();
+    // setUsers(data); 
+    // setLoading(false);
+  };
 
+  const searchUsers = async (text) => {
+    setLoading();
+    const params = new URLSearchParams({
+      q: text,
+    });
+    const response = await fetch(`${apiUrl}/search/users?${params}`, {
+      headers: {
+        authorization: `token ${apiKey}`,
+      },
+    });
+    const { items } = await response.json();
 
-        //dispatch function to update state
-        dispatch ({
-            type : 'GET_USERS',
-            payload : items,
-        })
-        // setUsers(data);
-        // setLoading(false);
-    }
+    //dispatch function to update state
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+    // setUsers(data);
+    // setLoading(false);
+  };
 
- const setLoading  =( ) => {
-        dispatch({
-            type : 'SET_LOADING'
-        })
- }
- 
- const clearUsersResults = () => {
-        dispatch({
-            type : 'CLEAR_SEARCH_RESULT'
-        })
- }
-    return (
-        <GithubContext.Provider value={{
-            users : state.users,
-            loading : state.loading,
-            searchUsers,
-            clearUsersResults
-        }}> 
-            {
-                children
-            }
+  const setLoading = () => {
+    dispatch({
+      type: "SET_LOADING",
+    });
+  };
 
-        </GithubContext.Provider>
-    )
-
-
-
-}
-
+  const clearUsersResults = () => {
+    dispatch({
+      type: "CLEAR_SEARCH_RESULT",
+    });
+  };
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,  
+        searchUsers,
+        clearUsersResults,
+        getUser,
+        repos : state.repos,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
 
 export default GithubContext;
